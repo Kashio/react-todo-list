9b6f0b7aa4a2915dcdc979290232af3404e04a93
Roy Mor <roy.mor173@gmail.com>
2019-03-31 14:03:50 +0300

    use TodoListContext to pass dispatch to TodoItem later on to use for DELETE and COMPLETE actions

--- client/src/components/todo-list/todo-list.jsx
+++ client/src/components/todo-list/todo-list.jsx
@@ -2,15 +2,32 @@ import React, {useContext, useReducer} from 'react';
 import './todo-list.scss';
 import reducer from '../../reducers/todos';
 import types from '../../actions/todos';
+import TodoItem from '@/todo-item/todo-item';
 
 const TodoListContext = React.createContext();
 
 const TodoList = () => {
-    const [todos, dispatch] = useReducer(reducer, [])
+    const [todos, dispatch] = useReducer(reducer, []);
 
-    return <ul className='todo-list'>
+    const add = () => {
+      dispatch({type: types.ADD});
+    };
 
-    </ul>;
+    const reset = () => {
+        dispatch({type: types.RESET});
+    };
+
+    return (
+        <TodoListContext.Provider value={dispatch}>
+            <div className='todo-list'>
+                {
+                    todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)
+                }
+            </div>
+            <button onClick={add}>Add</button>
+            <button onClick={reset}>Reset</button>
+        </TodoListContext.Provider>
+    );
 };
 
 export default TodoList;
